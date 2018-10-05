6c8f89453e05696f65306dfc5e895e097434d75a
Victor Abreu <victorsabreu@gmail.com>
2018-10-04 23:49:56 -0300

    Seeding service

--- SalesMVC/Models/Seller.cs
+++ SalesMVC/Models/Seller.cs
@@ -11,8 +11,8 @@ namespace SalesMVC.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public double BaseSalary { get; set; }
         public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
@@ -21,13 +21,13 @@ namespace SalesMVC.Models
 
         }
 
-        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
             Email = email;
-            BaseSalary = baseSalary;
             BirthDate = birthDate;
+            BaseSalary = baseSalary;
             Department = department;
         }
 
